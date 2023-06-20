3c88e225e91a51e69fe39da5b5e3619bab2ff428
animesh3640 <karne.ani@gmail.com>
2023-06-20 11:01:07 +0530

    s

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,7 +3,7 @@ import React from "react";
 const Child =({cartItems, onRemove })=>{
     
     return (
-        <div>
+        <div className="child">
             <h2>Child Component</h2>
             <ul>
                 {cartItems && cartItems.map((item) => (
